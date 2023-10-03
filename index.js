a2221d8f45300c04a584af4aa044d1352d983760
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-03 10:34:22 -0400

    started server on port 9000 and created a shortcut package script to restart the server with nodemon

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server');
-
-const port = 9000;
+const server = require('./api/server')
 
 // START YOUR SERVER HERE
+
+server.listen(9000, ()=>{
+    console.log('server started')
+})
