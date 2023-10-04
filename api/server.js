bbda8d5419dcba43bc4b5aa5297cf93b1cf6a188
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-03 20:39:00 -0400

    able to update the data on server

--- api/server.js
+++ api/server.js
@@ -33,10 +33,25 @@ server.get('/api/users/:id', async(req,res)=>{
         res.status(500).json({
             message: `Error happened when finding user ${req.params.id}`})
 
+            
     }
 })
 
 
+server.put('/api/users/:id', (req,res)=>{
+    try{
+        
+        const { id } = req.params;
+        Model.update(id, req.body)
+        console.log(req.body)
+        res.status(200).json({message: "it might have worked"})
+        
+    }catch(err){
+        res.status(500).json({
+            message: 'Error updating user'
+        })
+    }
+})
 
 
 
