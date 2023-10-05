63d35648b5d10a227a61b7412ac3ee910e5da221
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-05 10:35:13 -0400

    if user id not found when trying to make a put request to the server, the specified 404 message is displayed

--- api/server.js
+++ api/server.js
@@ -44,6 +44,9 @@ server.put('/api/users/:id', async (req,res)=>{
         
         const { id } = req.params;
        const updatedUser = await Model.update(id, req.body)
+       if(!updatedUser){
+        res.status(404).json({message: "The user with the specified ID does not exist"})
+       }
         console.log(req.body)
         res.status(200).json(
             {message: "user updated successfully",
