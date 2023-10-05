f5da05da34275bad4dd22b74504d52a31c489743
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-05 10:26:55 -0400

    propper 404 message displays on delete requests when id is not found

--- api/server.js
+++ api/server.js
@@ -65,6 +65,11 @@ server.delete('/api/users/:id', async(req,res)=>{
     try{
         const { id } = req.params;
         const deleted = await Model.remove(id)
+        if(!deleted){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
         res.status(200).json({
             message: `User with id ${id} was successfully removed`,
             deleted: deleted
